b91e1a52becc74ebb424423f2809bf8d28cb1c87
drusjs <magnojoky962@gmail.com>
2024-09-13 20:23:17 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -38,10 +38,61 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    let keywords = []
+    const keywordsFilter = document.querySelectorAll('.keyword-tags .btn')
+
+    keywordsFilter.forEach(btn=>{
+        btn.addEventListener('click', (e)=>{
+            e.currentTarget.classList.toggle('active')
+            if (e.currentTarget.classList.contains('active')) {
+                keywords.push(e.currentTarget.innerHTML)
+            } else {
+                keywords = keywords.filter((item) => item != e.currentTarget.innerHTML)
+            }
+            setKeywordsView()
+        })
+    })
+
+    function setKeywordsView() {
+        const container = document.querySelector('.btn-toggles')
+        const cards = document.querySelectorAll('.adventure-card')
+        cards.forEach(el=>{el.classList.add('filter-enable')})
+
+        if (keywords.length > 0) {
+            container.classList.add('keyword')
+            cards.forEach(el=>{
+                let itemKeys = el.dataset.keywords
+                let isInclud = false
+
+                keywords.forEach(item=>{
+                    if (itemKeys.includes(item)) {
+                        isInclud = true
+                    }
+                })
+
+                if (!isInclud) {
+                    el.classList.remove('filter-enable')
+                }
+            })
+        } else {
+            container.classList.remove('keyword')
+        }
+    }
+    
+
 });
 
-function diffFilterAction(enableClass, removeClasses) {
+function diffFilterAction(enableClass, removeClasses, el) {
     const container = document.querySelector('.btn-toggles')
+    const active = el.parentElement.querySelector('.active')
+
+    if (!el.classList.contains('active')) {
+        if (active) {
+            active.classList.remove('active')
+        }
+    }
+
+    el.classList.toggle('active')
 
     removeClasses.forEach((classname) => {
         container.classList.remove(classname)
